f1f6249e864c1f7887f124f6ab30c7f7fd95d00f
Soniya kumarasamy <158746279+soniyakumarasamy@users.noreply.github.com>
2024-05-10 18:12:10 +0800

    Update ScoreService.java

--- src/main/java/net/javaguides/springboot/service/ScoreService.java
+++ src/main/java/net/javaguides/springboot/service/ScoreService.java
@@ -16,31 +16,16 @@ public class ScoreService {
     private ScoreRepository scoreRepository;
      
     public boolean checkIfRecordExists(int score, String tiles) {
-    	System.out.println("********");
-        // Query the database to check if a record with the given score and tiles exists
         return scoreRepository.existsByScoreAndTiles(score, tiles);
     }
-    
 
     public void saveScore(int score,String tiles) {
-        // Create a Score entity
         Score scoreEntity = new Score();
-        System.out.println("%%%%%%%%%%%%%%%%%%%");
-        
-       //Score existingWord  =duplicateTilesremove.findByTiles(tiles);
-       /*System.out.println("&&&&&"+existingWord);
-      if (existingWord != null) {
-    	   existingWord.setTiles(tiles);
-           scoreRepository.save(existingWord);
-           // If the tiles word already exists, you may choose to update the score, or do nothing
-           // For now, let's assume you just return without saving again
-           return;
-       }*/
        
         scoreEntity.setScore(score);
         scoreEntity.setTiles(tiles);
 
-        // Save the score to the database
+
         scoreRepository.save(scoreEntity);
        
     }
